cd4e4c0892b8864159d1859a4fa39976f1026369
Mankarjasika <mankarjasi@email.com>
2026-06-26 15:03:50 +0530

    Day 3: classes and objects - finished is_passing, added describe method, understood self and object necessity

---
+++ week1-python-fundamentals/day03_class_obj_1.py
@@ -0,0 +1,27 @@
+class Student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+
+    def is_passing(self):
+        return self.marks >= 40
+    
+    def describe(self):
+        if self.is_passing():
+            status = "passing"
+        else:
+            status = "failing"
+        return f"Student {self.name} scored {self.marks} and is {status}"
+
+asha = Student("Asha",78)
+rohan = Student("Rohan",35)
+
+#print(asha.name,asha.marks,asha.is_passing())
+#print(rohan.name,rohan.marks,rohan.is_passing())
+
+#print(asha.is_passing())
+#print(rohan.is_passing())
+
+
+print(asha.describe())
+print(rohan.describe())
